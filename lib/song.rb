572e3b5da5582f8a6f1349f34cc6a069dfcdb647
Paul Leugemors <pleugemors@yahoo.com>
2019-12-11 08:35:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre 
+    @@count += 1 
+    @@artists << artist 
+  end
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count= 0
+  @@genres =[]
+  @@artists = []
+  
+  def self.count 
+    return @@count 
+  end
+  
+  def genres # returns array of uniqe genres 
+  
+  end
+  
+  def artists #returns array of all artists of existing songs 
+  
+  end
+end 
